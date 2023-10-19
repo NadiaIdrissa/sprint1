59f95af151f5ef3e9c835850ed161f6fd22f5094
Saskia Dessapes <fb191953@ens.uqam.ca>
2023-10-19 16:02:41 -0400

    Ajustement de la methode separerDonneePourUneIntervention

--- CsvParser.java
+++ CsvParser.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Cette classe sert a aller extraire les donnees contenues dans une fichiers CSV  afin d'en modeliser les
- * interventions survenues policieres survenues dans les parcs de Montreal
+ * Cette classe sert a aller extraire les donnees contenues dans un fichier CSV  afin d'en modeliser les
+ * interventions  policieres survenues dans les parcs de Montreal
  */
 public class CsvParser {
     /**
-     * Cette methode va creer une liste qui va extraire les donnees contenues dans le fichier CSV
-     *
+     * Cette methode va creer une liste qui va extraire les donnees contenues dans le fichier CSV par intervention
      * @param nomFichier
      * @return Une liste de toutes les interventions parvenues et enregistrees dans le fichier CSV
      */
@@ -28,24 +27,24 @@ public class CsvParser {
     }
     /**
      * Cette methode va separer les donnees date, heure, parc, arrondissement et description pour une intervention
-     * @param donnees
-     * @param index
-     * return le tableau contenant toutes le information pour une intervention
      *
+     * @param donnees
+     * return le tableau contenant toutes les informations pour l'intervention selectionnee(celle donnee en parametre)
      */
-    public static String [] extraireDonneePourUneIntervention(String donnees, int index) {
+    public static String [] separerDonneePourUneIntervention(String donnees) {
         String [] information = donnees.split(",");
         return information;
     }
     /**
      * Cette methode va creer une liste qui va contenir toutes les interventions contenues dans le fichier CSV
+     *
      * @param donnees
-     * @return la liste des interventions
+     * @return la liste des interventions qui etaient dans le fichier CSV
      */
     public static ArrayList<Intervention> convertirEnIntervention(ArrayList<String> donnees){
         ArrayList<Intervention> interventions = new ArrayList<>();
         for (int i = 1; i < donnees.size(); i++){
-            String [] information = extraireDonneePourUneIntervention(donnees.get(i), i);
+            String [] information = separerDonneePourUneIntervention(donnees.get(i));
 
             Intervention intervention = new Intervention(information[0],information[1],information[2],information[3],information[4]);
             interventions.add(intervention);
