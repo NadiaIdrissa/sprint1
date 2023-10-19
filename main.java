8a2582fe07b2c25ceab59e7afd73fa100e9a31f5
Yasmine <yasminecisse264@gmail.com>
2023-10-19 13:59:55 -0400

    modification de la branch main

--- main.java
+++ main.java
@@ -1,2 +1,42 @@
 public class main {
-}
+
+        private String date;
+        private String heure;
+        private String description;
+        private String parc;
+        private String arrondissement;
+        private int nbrIntervention;
+
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getHeure() {
+            return heure;
+        }
+
+        public String getParc() {
+            return parc;
+        }
+
+        public String getArrondissement() {
+            return arrondissement;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+
+        public int getNbrIntervention() {
+            return nbrIntervention;
+        }
+
+
+
+
+    }
+
+
+
