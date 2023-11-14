61186c225f8618103d1a5755ed08df1f6d824283
Nadia <moussa_idrissa.nadia@courrier.uqam.ca>
2023-11-14 15:16:42 -0500

    probleme java.lang resolu

--- src/main/java/ProgrammePrincipal.java
+++ src/main/java/ProgrammePrincipal.java
@@ -1,42 +1,32 @@
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 
 public class ProgrammePrincipal {
 
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Usage: java Entree.Main <fichier_entree.csv> <fichier_sortie.csv>");
-        } else {
-            Scanner scanner = new Scanner(System.in);
-            String entreeCSV = args[0];
-            String sortieCSV = args[1];
-            List<String> listArrondissement = Intervention.chargementArrondissementJSON();
-            System.out.println("fichier d'entrée (avec extension .csv) : ");
-            entreeCSV = scanner.nextLine();
-            if (!entreeCSV.endsWith(".csv")) {
-                System.out.println("Fichier introuvable - Fin du programme");
-            } else {
-                do {
-                    System.out.println("Fichier de sortie (avec extension .csv) : ");
-                    sortieCSV = scanner.nextLine();
-                } while (!sortieCSV.endsWith(".csv"));
-
-                scanner.close();
-
-                try {
-                    ArrayList<String> data = Entree.extraireDonnees(entreeCSV);
-                    ArrayList<Intervention> evenements = Entree.convertirEnIntervention(data);
-                    Sortie.sauvegarderListeDesInterventionsDansSortieCSV(evenements, sortieCSV);
-                    System.out.println("Exécution réussie. Les résultats sont enregistrés dans le fichier '" + sortieCSV + "'.");
-                } catch (FileNotFoundException e) {
-                    System.err.println("Erreur: Fichier introuvable - " + e.getMessage());
-                }
-
-            }
+            System.out.println("Fichier introuvable");
+            return;
         }
+        String entreeCSV = args[0];
+        String sortieCSV = args[1];
+
+        try {
+            ArrayList<String> data = Entree.extraireDonnees(entreeCSV);
+            ArrayList<Intervention> evenements = Entree.convertirEnIntervention(data);
+            Sortie.sauvegarderListeDesInterventionsDansSortieCSV(evenements, sortieCSV);
+            System.out.println("Exécution réussie. Les résultats sont enregistrés dans le fichier '" + sortieCSV + "'.");
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Erreur: Fichier introuvable - " + e.getMessage());
+        }catch (IOException e) {
+            System.err.println("Erreur d'entrée/sortie - " + e.getMessage());
+        }
+
+
     }
+
 }
