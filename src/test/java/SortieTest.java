d540560bad51cc2df3f3939d2c844b69789797a1
Saskia Dessapes <fb191953@ens.uqam.ca>
2023-11-14 16:25:33 -0500

    "Correction 1 conflit workspace.xml"

--- src/test/java/SortieTest.java
+++ src/test/java/SortieTest.java
@@ -1,7 +1,4 @@
-import org.junit.jupiter.api.Test;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 
 public class SortieTest {
