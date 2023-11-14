11a9dbe4c0f7738d573ab1816271ea1f212768d1
Yasmine <yasminecisse264@gmail.com>
2023-11-14 00:51:08 -0500

    Creation des classes EntreeTest et SortieTest

---
+++ src/test/java/EntreeTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EntreeTest {
+
+
+}
+
+
+
