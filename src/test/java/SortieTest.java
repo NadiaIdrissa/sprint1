a909180e417a25acaab28d801d91de58873ce831
Yasmine <yasminecisse264@gmail.com>
2023-11-14 22:54:21 -0500

    tests completé

--- src/test/java/SortieTest.java
+++ src/test/java/SortieTest.java
@@ -1,4 +1,11 @@
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SortieTest {
